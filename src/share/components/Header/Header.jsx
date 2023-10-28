2b39926bc20540e54597381762ea921e6bbb0911
DYlan <gownerbeats@gmail.com>
2023-10-28 14:12:37 -0500

    refact message

--- src/share/components/Header/Header.jsx
+++ src/share/components/Header/Header.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+const TEXTSNav = {
+    logo: 'FungusWork',
+    options: [
+        {name: 'Inicio', url: '#'},
+        {name: 'Nosotros', url: '#'},
+        {name: 'Servicios', url: '#'},
+        {name: 'Proyectos', url: '#'},
+    ],
+    contact: 'Contactanos',
+    contactURL: '#'
+}
+
 
 export default function Header() {
+
+    const [active, setActive] = useState(false);
+
+
+    const handleActive = () => {
+        setActive(true);
+    }
+
+
   return (
     <header className='px-4 py-6 flex flex-row items-center justify-between'>
         <div className='flex flex-row gap-4 items-center'>
@@ -8,25 +31,24 @@ export default function Header() {
                 src="/logo/logo.svg"
                 alt="Logo"
             />
-            <p className='text-2xl'>FungusWork</p>
+            <p className='text-2xl'>{TEXTSNav.logo}</p>
         </div>
-        <div className='flex flex-row items-center gap-6'>
+        <div
+            onClick={handleActive}
+            className='lg:hidden'
+        >
+            
+        </div>
+        <div className='hidden lg:flex flex-row items-center gap-6'>
             <ul className='flex flex-row gap-6 text-2xl'>
-                <li>
-                    <a href="#" className='h-full'>Inicio</a>
-                </li>
-                <li>
-                    <a href="#" className='my-auto'>Nosotros</a>
-                </li>
-                <li>
-                    <a href="#" className='my-auto'>Servicios</a>
-                </li>
-                <li>
-                    <a href="#" className='my-auto'>Proyectos</a>
-                </li>
+                {TEXTSNav.options.map((data, index)=>(
+                    <li key={index}>
+                        <a href={data.url} className='h-full'>{data.name}</a>
+                    </li>
+                ))}
             </ul>
-            <div className='bg-primary px-6 py-4 rounded-full'>
-                <a href="#">Contactanos</a>
+            <div className='bg-primary px-6 py-4 rounded-full text-lg'>
+                <a href={TEXTSNav.contactURL}>{TEXTSNav.contact}</a>
             </div>
         </div>
     </header>
