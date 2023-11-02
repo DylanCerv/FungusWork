6b84bebe2ad724d9883b96058bddf62a0a60dd85
BrayanCalle <bryan08_2011@hotmail.com>
2023-11-02 12:51:23 -0500

    Se añadio iconos a Nuesto-Equipo

--- src/pages/Nuestro-Equipo.jsx
+++ src/pages/Nuestro-Equipo.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import TeamMemberCard from '../components/TeamMemberCard';
+import photochica from '../img/photo-chica-2.jpg';
+import photochico from '../img/portrait-photography.jpg';
+import defaultimg from '../img/800x1000.png';
 
 export const NuestroEquipo = () => {
   // Datos de los miembros del equipo
@@ -7,24 +10,67 @@ export const NuestroEquipo = () => {
     {
       name: 'Jemmy Watson',
       role: 'Graphic Designer',
-    //   imageSrc: 'https://via.placeholder.com/800x1000',
-      imageSrc: 'https://images.unsplash.com/photo-1521119989659-a83eee488004?auto=format&fit=crop&q=80&w=1023&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      imageSrc: photochica,
       socialLinks: [
-        { url: 'https://www.facebook.com/', icon: 'fa-facebook-f' },
-        { url: 'https://twitter.com/', icon: 'fa-twitter' },
+        { url: 'https://www.facebook.com/', icon: 'FaFacebookF' },
+        { url: 'https://twitter.com/', icon: 'FaTwitter' },
+        { url: 'https://www.linkedin.com/', icon: 'FaLinkedinIn' },
         // Agrega otros enlaces aquí
       ],
     },
     {
       // Datos del segundo miembro del equipo
+      name: 'Jemmy Watson',
+      role: 'Graphic Designer',
+      imageSrc: photochico,
+      socialLinks: [
+        { url: 'https://www.facebook.com/', icon: 'FaFacebookF' },
+        { url: 'https://twitter.com/', icon: 'FaTwitter' },
+        { url: 'https://www.linkedin.com/', icon: 'FaLinkedinIn' },
+      ],
+      
     },
     // Agrega datos de otros miembros del equipo
+    {
+      // Datos del segundo miembro del equipo
+      name: 'Jemmy Watson',
+      role: 'Graphic Designer',
+      // imageSrc: 'https://via.placeholder.com/800x1000',
+      imageSrc: defaultimg,
+      socialLinks: [
+        { url: 'https://www.facebook.com/', icon: 'FaFacebookF' },
+        { url: 'https://twitter.com/', icon: 'FaTwitter' },
+        { url: 'https://www.linkedin.com/', icon: 'FaLinkedinIn' },
+      ],
+    },
+    {
+      // Datos del segundo miembro del equipo
+      name: 'Jemmy Watson',
+      role: 'Graphic Designer',
+      imageSrc: defaultimg,
+      socialLinks: [
+        { url: 'https://www.facebook.com/', icon: 'FaFacebookF' },
+        { url: 'https://twitter.com/', icon: 'FaTwitter' },
+        { url: 'https://www.linkedin.com/', icon: 'FaLinkedinIn' },
+      ],
+    },
+    {
+      // Datos del segundo miembro del equipo
+      name: 'Jemmy Watson',
+      role: 'Graphic Designer',
+      imageSrc: defaultimg,
+      socialLinks: [
+        { url: 'https://www.facebook.com/', icon: 'FaFacebookF' },
+        { url: 'https://twitter.com/', icon: 'FaTwitter' },
+        { url: 'https://www.linkedin.com/', icon: 'FaLinkedinIn' },
+      ],
+    }
   ];
 
   return (
     <div className="container mx-auto mt-10">
       <div className="flex flex-wrap justify-center">
-        <div className="w-full md:w-1/2 text-center margin-4-half-rem-bottom">
+        <div className="w-full md:w-1/2 text-center mb-8">
           <span className="alt-font text-extra-medium">The Development Team</span>
           <h5 className="alt-font text-extra-dark-gray font-weight-500 text-green-500">Creative people</h5>
         </div>
