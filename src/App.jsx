6c61b20f92f04445224abd6a8e31714d77f21955
Juan Camilo V.M <eljuankixx1354@hotmail.com>
2023-11-02 13:10:15 -0500

    Desarrollo de Aplicacion: - Modulo de Proyectos - Modulo de Servicios - Modulo de Footer

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ContactInfo } from './components/Contacts/ContactInfo';
+import { Footer } from './components/Footer/Footer';
+import { Services } from './components/Services/Services';
+
+function App() {
+  return (
+    <div className="h-screen bg-black">
+      <Services/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contacts/constants/data.js
@@ -0,0 +1,26 @@
+import { HiLocationMarker } from 'react-icons/hi'
+import { FiSmartphone } from 'react-icons/fi'
+import { AiOutlineMail, AiFillFile } from 'react-icons/ai'
+
+export const contacts = [
+    {
+        topLabel: 'Cretic Pro Agency 103',
+        botLabel: 'Mallin Street, Texas, USA',
+        icon: <HiLocationMarker/>
+    },
+    {
+        topLabel: '+1245201396',
+        botLabel: '+2245201396',
+        icon: <FiSmartphone/>
+    },
+    {
+        topLabel: 'support@rstheme.com',
+        botLabel: 'info@codesless.com',
+        icon: <AiOutlineMail/>
+    },
+    {
+        topLabel: '(+088) 589-8745',
+        botLabel: '(+088) 222-9999',
+        icon: <AiFillFile/>
+    }
+]
