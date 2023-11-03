9e8e33748aecada128446e108b9d6e87e1d9ad29
DYlan <gownerbeats@gmail.com>
2023-11-03 10:44:02 -0500

    refact code

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NuestroEquipo from './pages/Nuestro-Equipo';
 import Home from './pages/Home';
-import Header from './share/components/Header/Header';
+import NavBar from './components/NavBar/NavBar';
 import { Services } from './components/Services/Services'
 import { ContactInfo } from './components/Contacts/ContactInfo'
-import { Footer } from './components/Footer'
+import { Footer } from './components/Footer/Footer';
 import { useState } from 'react';
 import { categories, data } from './data/data';
 import { ButtonList, CardProyectos } from './components';
@@ -39,55 +39,14 @@ function App() {
 
   return (
     <div className="h-screen">
-      <main>
-        <section className='relative !py-0'>
-        <Header />
+        <NavBar />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={< Home />} /> {/* Nueva página */}
             <Route path="/nuestro-equipo" element={< NuestroEquipo />} /> {/* Nueva página */}
           </Routes>
         </BrowserRouter>
-          <img
-            className='absolute -z-10 top-0 left-0 w-full'
-            src="/img/bg-top.svg"
-            alt="bg"
-          />
-          <img
-            className='absolute -z-10 bottom-0 left-0 w-full'
-            src="/img/bg-bottom.svg"
-            alt="bg"
-          />
-          <article className='h-[40vh] sm:h-[50vh] py-20 px-4 text-center text-6xl md:text-start md:text-7xl md:px-20 md:h-[90vh] lg:text-8xl lg:h-[100vh] xl:h-screen'>
-            <h1 className='font-semibold italic text-white'>{TEXTS.Section1.titleP}</h1>
-            <h1 className='text-secundary'>{TEXTS.Section1.titleS}</h1>
-          </article>
-        </section>
-        <section className='py-20 px-10 flex flex-col gap-10 items-center justify-center text-center md:px-20 lg:p-40'>
-          <img
-            src="/logo/logo2.svg"
-            alt="Logo"
-          />
-          <h1 className='text-4xl text-white'>{TEXTS.Section2.title}</h1>
-          <div>
-            <p className='text-2xl text-white'>{TEXTS.Section2.description1}</p>
-            <p className='text-2xl text-white'>{TEXTS.Section2.description2}</p>
-          </div>
-        </section>
-        <section>
-          <div className="title">
-            <h1 className="text-white font-bold">Proyectos</h1>
-          </div>
-
-          <ButtonList categories={AllCategories} filterCategory={filterCategory} selectedCategory={selectedCategory} />
-          <div className= "container mx-auto">
-            <CardProyectos projects={projects} />
-          </div>
-        </section>
-        <Services/>
-        <ContactInfo/>
         <Footer/>
-      </main>
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 
 const TEXTSNav = {
-    logo: 'FungusWork',
+    logo: {name: 'FungusWork', url: '/'},
     options: [
         {name: 'Inicio', url: '/'},
         {name: 'Nosotros', url: '/nuestro-equipo'},
@@ -25,34 +25,36 @@ export default function NavBar() {
 
 
   return (
-    <header className='px-10 py-6 flex flex-row items-center justify-between'>
-        <Logo/>
-        {!active && 
-            <Hamburger
-                src={"/icons/hamburger.svg"}
-                onClick={handleActive}
-            />
-        }
-        <div className={`bg-black absolute left-0 top-0 pt-6 px-10 w-full h-screen flex flex-col justify-start !gap-10 ${active ? '!fixed' : 'hidden'} lg:bg-opacity-0 lg:!static lg:h-fit lg:flex lg:flex-row lg:justify-end lg:!w-fit items-center gap-6`}>
-            <div className='lg:hidden flex items-center justify-between w-full'>
-                <Logo/>
+    <header className='absolute top-0 z-[100] w-full'>
+        <nav className='px-10 py-6 flex flex-row items-center justify-between'>
+            <Logo/>
+            {!active && 
                 <Hamburger
-                    className={'right-10 top-5'}
-                    src={"/icons/close.svg"}
+                    src={"/icons/hamburger.svg"}
                     onClick={handleActive}
                 />
+            }
+            <div className={`bg-black absolute left-0 top-0 pt-6 px-10 w-full h-screen flex flex-col justify-start !gap-10 ${active ? '!fixed' : 'hidden'} lg:bg-opacity-0 lg:!static lg:h-fit lg:flex lg:flex-row lg:justify-end lg:!w-fit items-center gap-6`}>
+                <div className='lg:hidden flex items-center justify-between w-full'>
+                    <Logo/>
+                    <Hamburger
+                        className={'right-10 top-5'}
+                        src={"/icons/close.svg"}
+                        onClick={handleActive}
+                    />
+                </div>
+                <ul className='mr-auto lg:mr-0 mt-10 lg:my-auto flex flex-col lg:flex-row gap-6 text-2xl font-semibold'>
+                    {TEXTSNav.options.map((data, index)=>(
+                        <li key={index}>
+                            <a href={data.url} className='h-full text-white'>{data.name}</a>
+                        </li>
+                    ))}
+                </ul>
+                <div className='mr-auto lg:mr-0 bg-primary px-6 py-3 rounded-full text-xl font-semibold text-white'>
+                    <a href={TEXTSNav.contactURL}>{TEXTSNav.contact}</a>
+                </div>
             </div>
-            <ul className='mr-auto lg:mr-0 mt-10 lg:my-auto flex flex-col lg:flex-row gap-6 text-2xl font-semibold'>
-                {TEXTSNav.options.map((data, index)=>(
-                    <li key={index}>
-                        <a href={data.url} className='h-full text-white'>{data.name}</a>
-                    </li>
-                ))}
-            </ul>
-            <div className='mr-auto lg:mr-0 bg-primary px-6 py-3 rounded-full text-xl font-semibold text-white'>
-                <a href={TEXTSNav.contactURL}>{TEXTSNav.contact}</a>
-            </div>
-        </div>
+        </nav>
     </header>
   )
 }
@@ -60,14 +62,17 @@ export default function NavBar() {
 
 function Logo () {
     return (
-        <div className='flex flex-row gap-4 items-end'>
-            <img
-                className='w-14'
-                src="/logo/Fongus_LogoBlanco.svg"
-                alt="Logo"
-            />
-            <p className='text-2xl font-bold tracking-wider'>{TEXTSNav.logo}</p>
-        </div>
+        <a href={TEXTSNav.logo.url}>
+            <div className='flex flex-row gap-4 items-end text-white'>
+                <img
+                    className='w-14'
+                    src="/logo/Fongus_LogoBlanco.svg"
+                    alt="Logo"
+                />
+                <p className='text-2xl font-bold tracking-wider'>{TEXTSNav.logo.name}</p>
+            </div>
+        </a>
+
     )
 }
 
