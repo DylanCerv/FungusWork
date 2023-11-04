1e1da145b8652ac8a5c23d3b6cf5e7d971205924
Juan Camilo V.M <eljuankixx1354@hotmail.com>
2023-11-04 15:50:49 -0500

    - Arreglos en Responsive mas Pequeño

--- src/components/Contacts/ContactInfo.jsx
+++ src/components/Contacts/ContactInfo.jsx
@@ -9,7 +9,7 @@ export const ContactInfo = () => {
         <div id='contacto' className={style.contact_container}>
             <div className='bg-green-100 flex flex-col justify-center gap-4 md:w-contact-width h-contact-height rounded-2xl px-14'>
                 <span className='text-3xl font-bold text-black'>Contact Info</span> 
-                <span className='font-semibold text-black'>
+                <span className='text-lg sm:text-md font-semibold text-black'>
                 Objectively innovate your empowered
                 manufactured products whereas parallel
                 platforms for your ideas.
@@ -22,15 +22,15 @@ export const ContactInfo = () => {
             </div>
             <div className='flex flex-col items-center justify-center gap-8 pb-16'>
                 <span className='text-4xl font-bold text-green-500'>Contactanos</span>
-                <div className='flex justify-between gap-5'>
-                    <InputText label='What’s your name?' placeholder='Full name' className='w-64 md:w-96'/>
-                    <InputText label='What’s your email address?' placeholder='Email address' className='w-64 md:w-96'/>
+                <div className='flex flex-col sm:flex-row justify-between gap-5'>
+                    <InputText label='What’s your name?' placeholder='Full name' className='w-80 md:w-96'/>
+                    <InputText label='What’s your email address?' placeholder='Email address' className='w-80 md:w-96'/>
                 </div>
-                <div className='flex justify-between gap-5'>
-                    <InputText label='What’s your phone number?' placeholder='Phone number' className='w-64 md:w-96'/>
-                    <InputText label='Email subject' placeholder='Subject' className='w-64 md:w-96'/>
+                <div className='flex flex-col sm:flex-row justify-between gap-5'>
+                    <InputText label='What’s your phone number?' placeholder='Phone number' className='w-80 md:w-96'/>
+                    <InputText label='Email subject' placeholder='Subject' className='w-80 md:w-96'/>
                 </div>
-                <textarea name="" id="" rows="10" className="w-108 md:w-120 p-4 text-white resize-none rounded-xl"></textarea>
+                <textarea name="" id="" rows="10" className="w-100 sm:w-108 md:w-120 p-4 text-white resize-none rounded-xl"></textarea>
                 <div className='flex justify-start w-full md:w-120'>
                     <button className='bg-green-500 text-green-100 text-xl font-bold text-gray w-52 h-14 rounded-full'>Send Message</button>
                 </div>
