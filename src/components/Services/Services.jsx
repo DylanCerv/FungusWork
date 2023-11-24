6b02c56d95449336fae59c51114484617fc2f209
DYlan <gownerbeats@gmail.com>
2023-11-24 14:52:19 -0500

    textos de la seccion servicios

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,3 +1,4 @@
+import { DATA_SERVICES } from "../../data/dataServicios"
 import { Service } from "./Service"
 
 export const Services = () => {
@@ -6,34 +7,17 @@ export const Services = () => {
             <div className="flex flex-col items-center gap-8">
                 <span className="text-white text-3xl font-bold" data-aos="zoom-in" data-aos-duration="500">Servicios</span>
                 <div className="flex flex-wrap items-center justify-center gap-16 md:w-services-container sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl w-full overflow-hidden">
-                    <Service 
-                        tittle="Tecnologia & Desarrollo" 
-                        description="Analizamos y comprendemos los requerimientos de nuestros clientes para diseñar y desarrollar soluciones eficientes, con una interfaz de usuario bien diseñada, en combinación con una sólida  funcionalidad y una arquitectura robusta."
-                        href='test.com'
-                        animacion="fade-down-right"
-                        duracion="1000"
-                    />
-                    <Service 
-                        tittle="Tecnologia & Desarrollo" 
-                        description="Analizamos y comprendemos los requerimientos de nuestros clientes para diseñar y desarrollar soluciones eficientes, con una interfaz de usuario bien diseñada, en combinación con una sólida  funcionalidad y una arquitectura robusta."
-                        href='test.com'
-                        animacion="fade-down-left"
-                        duracion="1500"
-                    />
-                    <Service 
-                        tittle="Tecnologia & Desarrollo" 
-                        description="Analizamos y comprendemos los requerimientos de nuestros clientes para diseñar y desarrollar soluciones eficientes, con una interfaz de usuario bien diseñada, en combinación con una sólida  funcionalidad y una arquitectura robusta."
-                        href='test.com'
-                        animacion="fade-up-right"
-                        duracion="2000"
-                    />
-                    <Service 
-                        tittle="Tecnologia & Desarrollo" 
-                        description="Analizamos y comprendemos los requerimientos de nuestros clientes para diseñar y desarrollar soluciones eficientes, con una interfaz de usuario bien diseñada, en combinación con una sólida  funcionalidad y una arquitectura robusta."
-                        href='test.com'
-                        animacion="fade-up-left"
-                        duracion="2500"
-                    />
+                    {DATA_SERVICES.map((data, index)=>(
+                        <Service 
+                            key={index}
+                            src={data.image}
+                            tittle={data.title}
+                            description={data.description}
+                            href={data.href}
+                            animacion={data.animacion}
+                            duracion="1000"
+                        />
+                    ))}
                 </div>
             </div>
         </div>
---
+++ src/data/dataServicios.js
@@ -0,0 +1,52 @@
+const Anucia_redes_sociales = '/icons/services/icon _bullhorn_.svg';
+const diseño_Creatividad = '/icons/services/icon _Pen Nib_.svg';
+const paginaWeb = '/icons/services/icon _Laptop Code_.svg';
+const codeQR = '/icons/services/icon _qrcode_.svg';
+const repeat = '/icons/services/icon _repeat_.svg';
+const webMaster = '/icons/services/icon _shopping-cart_.svg';
+
+
+export const DATA_SERVICES = [
+    {
+        image: paginaWeb,
+        title: 'Páginas Web',
+        description: 'Analizamos y comprendemos los requerimientos de nuestros clientes para diseñar y desarrollar soluciones eficientes, con una interfaz de usuario bien diseñada, en combinación con una sólida  funcionalidad y una arquitectura robusta.',
+        href: '',
+        animacion: 'fade-down-right',
+    },
+    {
+        image: diseño_Creatividad,
+        title: 'Diseño Visual y Creatividad Online',
+        description: 'Eleva tu presencia digital con nuestras opciones de diseño. Desde post cautivadores hasta proyectos visuales a medida. Descubre cómo podemos potenciar tu imagen online.',
+        href: '',
+        animacion: 'fade-down-left',
+    },
+    {
+        image: Anucia_redes_sociales,
+        title: 'Anuncia con Impacto en  Redes Sociales',
+        description: 'Potencia tu visibilidad online con nuestro servicio SEM. Desde campañas personalizadas hasta configuración de anuncios. ¡Optimiza tu presencia y alcanza a tu audiencia clave!',
+        href: '',
+        animacion: 'fade-down-right',
+    },
+    {
+        image: codeQR,
+        title: 'Códigos QR a Medida',
+        description: 'Impulsa tu presencia con paquetes de códigos QR. Desde básicos hasta personalizados, facilitando la interacción con tu audiencia.',
+        href: '',
+        animacion: 'fade-down-left',
+    },
+    {
+        image: repeat,
+        title: 'Comunity Pro',
+        description: 'Impulsa tu presencia en redes con nuestros paquetes. Desde gestión básica hasta completa en múltiples plataformas. ¡Conéctate con tu audiencia de manera efectiva!',
+        href: '',
+        animacion: 'fade-down-right',
+    },
+    {
+        image: webMaster,
+        title: 'Webmaster Todo en Uno',
+        description: 'Descubre nuestros paquetes de gestión web. Desde blogs hasta eCommerce, optimizamos tu presencia online. ¡Delega el mantenimiento y enfócate en tu negocio!',
+        href: '',
+        animacion: 'fade-down-left',
+    },
+];
